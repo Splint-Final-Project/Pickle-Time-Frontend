935f986d66bf284f39e0d2eb8d5e91fee7184666
parkhyeonwoo <pq120wal@naver.com>
2024-05-25 22:24:17 +0900

    feat [박현우] : Input 컴포넌트 생성

--- src/components/commons/inputs/Input.tsx
+++ src/components/commons/inputs/Input.tsx
@@ -1,76 +1,18 @@
-import { FieldPath, FieldValues, UseControllerProps, useController, useForm } from 'react-hook-form';
-import PasswordInput from './PasswordInput';
-import { useEffect, useRef } from 'react';
-
+import React from 'react';
+//TODO : 스타일 컴포넌트로 만들기
 interface InputProps {
+  type: string;
   id: string;
-  label: string;
   placeholder: string;
-  focus?: boolean;
+  value: string;
+  onChange: () => void;
 }
 
-export function Input<
-  TFieldValues extends FieldValues = FieldValues,
-  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
->({ id, label, placeholder, focus, ...controllerProps }: InputProps & UseControllerProps<TFieldValues, TName>) {
-  const {
-    field: { value, onChange },
-    fieldState: { error },
-  } = useController(controllerProps);
-
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  useEffect(() => {
-    if (focus && inputRef.current) {
-      inputRef.current.focus();
-    }
-  }, []);
-
-  return (
-    <div>
-      <label htmlFor={id}>{label}</label>
-      <input type="text" id={id} value={value} onChange={onChange} placeholder={placeholder} ref={inputRef} />
-      <p>{error?.message}</p>
-    </div>
-  );
+function Input(
+  { type, id, onChange, placeholder, value }: InputProps,
+  ref: React.LegacyRef<HTMLInputElement> | undefined,
+) {
+  return <input type={type} id={id} onChange={onChange} placeholder={placeholder} value={value} ref={ref} />;
 }
 
-// type TUserInput = {
-//   userId: string;
-//   password: string;
-// };
-
-// export default function Form() {
-//   const { control, handleSubmit } = useForm<TUserInput>({
-//     defaultValues: {
-//       userId: '',
-//     },
-//     mode: 'onChange',
-//   });
-//   const onSubmit = data => {
-//     console.log(data);
-//   };
-
-//   return (
-//     <form onSubmit={handleSubmit(onSubmit)}>
-//       <Input
-//         name="userId"
-//         control={control}
-//         rules={{
-//           required: '아이디를 입력해주세요',
-//           pattern: {
-//             value: /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
-//             message: '이메일 형식을 지켜주세요',
-//           },
-//         }}
-//         placeholder="아이디 입력좀"
-//         id="userId"
-//         label="아이디"
-//         focus
-//       />
-//       <PasswordInput name="password" control={control} id="password" label="비번입력" placeholder="비번입력좀" />
-
-//       <button type="submit">제출</button>
-//     </form>
-//   );
-// }
+export default React.forwardRef(Input);
