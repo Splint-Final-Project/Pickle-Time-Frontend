8ddd8f68551f07e06f48b1ccd21c3c79c36c740e
MinJoon_Choi <minjoon1998@gmail.com>
2024-05-25 19:20:07 +0900

    chore [최민준] : 지도 마커 임시 목데이터 추가

--- src/components/map/KaKaoMap.tsx
+++ src/components/map/KaKaoMap.tsx
@@ -1,6 +1,7 @@
 import { useGeolocation } from '@/hooks/useGeolocation';
 import React from 'react';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
+import markerData from '../../mocks/markerData';
 
 const geolocationOptions = {
   enableHighAccuracy: true,
@@ -24,8 +25,13 @@ export default function KaKaoMap() {
           level={3}
         >
           <MapMarker position={{ lat: location.latitude, lng: location.longitude }}>
-            <div style={{ color: '#000' }}>Hello World!</div>
+            <div style={{ color: '#000' }}>현재 위치</div>
           </MapMarker>
+          {markerData.map(marker => (
+            <MapMarker key={marker.id} position={{ lat: marker.latitude, lng: marker.longitude }} title={marker.name}>
+              <div style={{ color: '#000' }}>{marker.name}</div>
+            </MapMarker>
+          ))}
         </Map>
       ) : (
         <div>지도 로딩중</div>
