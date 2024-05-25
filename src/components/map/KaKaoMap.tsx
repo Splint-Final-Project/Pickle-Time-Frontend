3240c01e9ee04996e4e102fcf7552457cc2732ef
MinJoon_Choi <minjoon1998@gmail.com>
2024-05-25 22:22:41 +0900

    fix [최민준] : 처음 지도 렌더링 시 현재 위치로 고정 안되던 오류 수정
    marker.name을 작성하면 center 오류가 발생, 이를 해결하기 위해 name을 적지 않거나 map이 렌더링 된 후 현재위치가 가운데로 지정되도록 center 지정 후 setCenter를 실행

--- src/components/map/KaKaoMap.tsx
+++ src/components/map/KaKaoMap.tsx
@@ -1,7 +1,9 @@
 import { useGeolocation } from '@/hooks/useGeolocation';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import markerData from '../../mocks/markerData';
+// import image from '/public/images/temporaryMarkerImage.png';
+import image from '/public/images/hing.png';
 
 const geolocationOptions = {
   enableHighAccuracy: true,
@@ -11,28 +13,61 @@ const geolocationOptions = {
 
 export default function KaKaoMap() {
   const { location, error } = useGeolocation(geolocationOptions);
+  const [map, setMap] = useState<kakao.maps.Map | null>(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    if (location) {
+      setIsLoaded(true);
+      // if (map) {
+      //   const center = new kakao.maps.LatLng(location.latitude, location.longitude);
+      //   map.setCenter(center);
+      // }
+    }
+    // }, [location,map]);
+  }, [location]);
 
   if (error) {
     return <div>에러 발생: {error}</div>;
   }
 
+  const moveToCurrentLocation = () => {
+    if (location && map) {
+      const center = new kakao.maps.LatLng(location.latitude, location.longitude);
+      map.setCenter(center);
+    }
+  };
+
   return (
     <div>
-      {location ? (
-        <Map
-          center={{ lat: location.latitude, lng: location.longitude }}
-          style={{ width: '100%', height: '400px' }}
-          level={3}
-        >
-          <MapMarker position={{ lat: location.latitude, lng: location.longitude }}>
-            <div style={{ color: '#000' }}>현재 위치</div>
-          </MapMarker>
-          {markerData.map(marker => (
-            <MapMarker key={marker.id} position={{ lat: marker.latitude, lng: marker.longitude }} title={marker.name}>
-              <div style={{ color: '#000' }}>{marker.name}</div>
+      {isLoaded ? (
+        <>
+          <Map
+            center={{ lat: location.latitude, lng: location.longitude }}
+            style={{ width: '100%', height: '400px' }}
+            level={3}
+            onCreate={setMap}
+          >
+            <MapMarker position={{ lat: location.latitude, lng: location.longitude }}>
+              <div style={{ color: '#000' }}>현재 위치</div>
             </MapMarker>
-          ))}
-        </Map>
+            {markerData.map(marker => (
+              <MapMarker
+                key={marker.id}
+                position={{ lat: marker.latitude, lng: marker.longitude }}
+                title={marker.name}
+                image={{ src: image, size: { width: 30, height: 30 } }}
+              >
+                {/* name을 적으면 Map의 center에서 오류가 발생함, name이 적용할 경우 위에 있는 useEffect 코드에 있는 주석 부분으로 수정해야함 */}
+                {/* <div style={{ color: '#000' }}>{marker.name}</div> */}
+              </MapMarker>
+            ))}
+          </Map>
+          <button style={{ border: '1px solid black' }} onClick={moveToCurrentLocation}>
+            현재 위치로 이동
+          </button>
+        </>
       ) : (
         <div>지도 로딩중</div>
       )}
