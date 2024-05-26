747722644a61369da9099df7f1f03f53a1d4c2bf
sihyonn <sihyeon982@naver.com>
2024-05-27 00:16:58 +0900

    feat: 공통 검색바 컴포넌트 구현

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,12 +1,25 @@
+import SearchBar from '@/components/common/SearchBar';
 import Button from '@/components/common/button/Button';
 import { BUTTON_TYPE } from '@/constants/BUTTON';
+import routes from '@/constants/routes';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
 
 export default function SignUp() {
+  const [searchParams] = useSearchParams();
+  const [keyword, setKeyword] = useState(searchParams.get('keyword'));
+
+  useEffect(() => {
+    setKeyword(searchParams.get('keyword'));
+  }, [searchParams]);
+
   return (
     <div>
       회원가입
       <Button>PRIMARY</Button>
       <Button styleType={BUTTON_TYPE.SECONDARY}>SECONDARY</Button>
+      <SearchBar placeholder="장소를 검색해보세요" uri={routes.signUp} />
+      <p>"{keyword}"를 검색한거 맞으시죵?</p>
     </div>
   );
 }
