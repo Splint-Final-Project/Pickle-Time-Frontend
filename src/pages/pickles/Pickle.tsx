d341c7fffd579adcd3b12607b7cc8e648de0ead3
vinoankr <vinoankr@gmail.com>
2024-05-28 16:32:48 +0900

    ㅇㅇ

--- src/pages/pickles/Pickle.tsx
+++ src/pages/pickles/Pickle.tsx
@@ -36,6 +36,7 @@ export default function Pickle() {
       //notified http status에 따라 분기.
       //OK의 경우에는 성공했다고 띄우고 피클 페이지로 이동(신청버튼이 '신청함'으로 바뀌고비활성화됨)
       //실패의 경우에는 실패했다고 띄우고 다시 그 피클 페이지
+      //같은 작업을 redirect url에서도 해야함
       alert('피클 신청에 성공하였습니다?');
     });
   }
