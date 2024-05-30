3582260b791e2f9da45510cec21a1740ef539d25
parkhyeonwoo <pq120wal@naver.com>
2024-05-30 21:51:34 +0900

    fix [박현우] : 폴더이름 forms => inputs로 변경

--- src/components/commons/forms/Label.tsx
+++
@@ -1,9 +0,0 @@
-interface LabelProps {
-  children: React.ReactNode;
-  htmlFor: string;
-}
-
-//TODO : 스타일 컴포넌트로 만들기
-export default function Label({ children, htmlFor }: LabelProps) {
-  return <label htmlFor={htmlFor}>{children}</label>;
-}
