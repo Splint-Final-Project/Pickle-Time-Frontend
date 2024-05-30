dddcf8d991b37cdf4c4738d22695595df3610066
parkhyeonwoo <pq120wal@naver.com>
2024-05-30 21:52:30 +0900

    feat [박현우] : input컴포넌트 분리

---
+++ src/components/common/inputs/Input.tsx
@@ -0,0 +1,20 @@
+import styled from '@emotion/styled';
+import React from 'react';
+//TODO : 스타일 컴포넌트로 만들기
+interface InputProps {
+  type: string;
+  id: string;
+  placeholder?: string;
+  value: string;
+  onChange: () => void;
+}
+
+function Input({ ...inputProps }: InputProps, ref: React.LegacyRef<HTMLInputElement> | undefined) {
+  return <Styled.Input {...inputProps} ref={ref} />;
+}
+
+export default React.forwardRef(Input);
+//TODO : 스타일링 추가 및 변경
+const Styled = {
+  Input: styled.input``,
+};
