8380dff44e531f7d8a9a53395a38c1fc84c039ea
sihyonn <sihyeon982@naver.com>
2024-05-30 20:19:40 +0900

    chore: 기존 signup에서 테스트하던 검색바 pickle 컴포넌트로 이동

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,25 +1,103 @@
-import SearchBar from '@/components/common/SearchBar';
-import Button from '@/components/common/button/Button';
-import { BUTTON_TYPE } from '@/constants/BUTTON';
-import routes from '@/constants/routes';
-import { useEffect, useState } from 'react';
-import { useSearchParams } from 'react-router-dom';
+import { authRequests } from '@/apis/index';
+import { SignUpFormValues } from '@/apis/types/authTypes';
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
-  const [searchParams] = useSearchParams();
-  const [keyword, setKeyword] = useState(searchParams.get('keyword'));
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { isSubmitting, isValid, errors },
+  } = useForm<SignUpFormValues>();
 
-  useEffect(() => {
-    setKeyword(searchParams.get('keyword'));
-  }, [searchParams]);
-
+  const [revealPw, setRevealPw] = useState(false);
+  const [revealConfirmPw, setRevealConfirmPw] = useState(false);
+  const navigate = useNavigate();
+  async function handleSignUp(data: SignUpFormValues) {
+    try {
+      await authRequests.signUp(data);
+      alert('회원가입 마무리 단계 페이지로 리다이렉팅됩니다');
+      navigate('/sign-up2');
+    } catch (e) {
+      console.log(e);
+    }
+  }
   return (
     <div>
       회원가입
-      <Button>PRIMARY</Button>
-      <Button styleType={BUTTON_TYPE.SECONDARY}>SECONDARY</Button>
-      <SearchBar placeholder="장소를 검색해보세요" uri={routes.signUp} />
-      <p>"{keyword}"를 검색한거 맞으시죵?</p>
+      <form
+        autoComplete="off"
+        onSubmit={handleSubmit(handleSignUp)}
+        style={{ display: 'flex', flexDirection: 'column' }}
+      >
+        <fieldset disabled={isSubmitting}>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              type="email"
+              placeholder="이메일을 입력해 주세요"
+              {...register('email', {
+                required: true,
+                pattern: {
+                  value: /\S+@\S+\.\S+/,
+                  message: '이메일 형식으로 작성해 주세요.',
+                },
+              })}
+            />
+          </div>
+          <div>
+            <label htmlFor="password">비밀번호</label>
+            <input
+              id="password"
+              type={revealPw ? 'text' : 'password'}
+              placeholder="8자 이상 입력해 주세요"
+              {...register('password', {
+                required: true,
+                minLength: {
+                  value: 8,
+                  message: '8자 이상 입력해 주세요.',
+                },
+              })}
+            />
+            <img
+              onClick={() => setRevealPw(prev => !prev)}
+              src={revealPw ? '/icons/hide.svg' : '/icons/reveal.svg'}
+              alt="reveal"
+            />
+            {errors.password && <span>{errors.password.message?.toString()}</span>}
+          </div>
+          <div>
+            <label htmlFor="checkPassword">비밀번호 확인</label>
+            <input
+              id="checkPassword"
+              type={revealConfirmPw ? 'text' : 'password'}
+              placeholder="비밀번호를 한번 더 입력해 주세요"
+              {...register('checkPassword', {
+                required: true,
+                validate: v => v === getValues('password') || '비밀번호가 일치하지 않습니다.',
+              })}
+            />
+            <img
+              onClick={() => setRevealConfirmPw(prev => !prev)}
+              src={revealConfirmPw ? '/icons/hide.svg' : '/icons/reveal.svg'}
+              alt="reveal"
+            />
+            {errors.checkPassword && <span>{errors.checkPassword.message?.toString()}</span>}
+          </div>
+          <button type="submit" disabled={!isValid}>
+            이메일로 시작하기
+          </button>
+        </fieldset>
+      </form>
+      <br />
+      <br />
+      <br />
+      <br />
+      <button>카카오로 시작하기</button>
+      <button disabled>네이버로 시작하기</button>
     </div>
   );
 }
