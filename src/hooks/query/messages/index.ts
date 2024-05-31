8620964be108fb992c3972a88f616ccf0a959840
whai2 <whai2125@gmail.com>
2024-05-31 16:53:59 +0900

    feat: 메시지를 위한 뮤테이션 훅 생성

--- src/hooks/query/messages/index.ts
+++ src/hooks/query/messages/index.ts
@@ -1,4 +1,4 @@
-import { useQuery, useMutation } from "@tanstack/react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { MessageType } from "@/apis/types/messages.type";
 
 import { messages } from "@/apis/index";
@@ -7,5 +7,15 @@ export const useGetMessages = (data: MessageType, receiverId: string) => {
   return useQuery({
     queryKey: ['messages', receiverId],
     queryFn: async () => await messages.get(data, receiverId),
+  })
+};
+
+export const useSendMessage = (data: MessageType, receiverId: string) => {
+  const queryClient = useQueryClient();
+  
+  return useMutation({
+    mutationFn: async () => await messages.send(data, receiverId),
+    onSuccess: () => queryClient.invalidateQueries({ queryKey: ['messages', receiverId] }),
+    onError: (error) => console.error('에러발생: ' + error),
   })
 }
