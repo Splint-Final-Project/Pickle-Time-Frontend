b10a9000aedcae7b3469bf93748f170eea930840
whai2 <whai2125@gmail.com>
2024-05-31 11:42:47 +0900

    delete: 불필요한 전역 상태 제거

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,5 @@
 import styled from '@emotion/styled';
 import { Link } from 'react-router-dom';
-import useButtonClick from '@/hooks/zustand/test';
 import useAuth from '@/hooks/zustand/useAuth';
 import KaKaoMap from '@/components/map/KaKaoMap';
 import routes from '@/constants/routes';
@@ -10,10 +9,10 @@ const Button = styled.button`
 `;
 
 export default function Home() {
-  const { count, inc } = useButtonClick();
-
+  // 전역 상태
   const { getUser, signOut } = useAuth();
   const user = getUser();
+
   return (
     <div>
       <h1>피클타임 홈 페이지</h1>
@@ -28,7 +27,6 @@ export default function Home() {
       </div>
       <br />
       <br />
-      <Button onClick={inc}>Styled Button {count}</Button>
       <div style={{ display: 'flex', flexDirection: 'column' }}>
         <Link to={routes.chatList}>Chat List</Link>
 
