0b132f079b46c1977e9c2177b404b5c8929e597c
whai2 <whai2125@gmail.com>
2024-05-31 17:47:20 +0900

    feat(message Style): 대화창 임시 구현, 스타일 적용

--- src/pages/chat/ConversationList.tsx
+++ src/pages/chat/ConversationList.tsx
@@ -5,9 +5,9 @@ import Conversation from './Conversation';
 export default function ConversationList() {
   return (
     <div>
-      <h1>채팅 목록 페이지입니다</h1>
+      {/* <h1>채팅 목록 페이지입니다</h1> */}
       <Conversation/>
-      <Link to="/">홈으로</Link>
+      {/* <Link to="/">홈으로</Link> */}
     </div>
   );
 }
