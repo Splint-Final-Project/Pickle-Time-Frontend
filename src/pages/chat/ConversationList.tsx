6ccdf2c4cfe5def872c656ac9b28bbada61ad6d2
whai2 <whai2125@gmail.com>
2024-06-01 03:37:06 +0900

    refactor : 리액트 쿼리 뮤테이션 적용

--- src/pages/chat/ConversationList.tsx
+++ src/pages/chat/ConversationList.tsx
@@ -6,35 +6,13 @@ import { useGetMessages, useSendMessage } from '@/hooks/query/messages';
 export default function ConversationList() {
   //server state
   const { data } = useGetMessages("2");
-  const { mutate } = useSendMessage({message: "hi"}, "1");
+  const { mutate } = useSendMessage({message: "hi"}, "2");
 
-  const handler = async () => {
-    const token = localStorage.getItem('token');
-    console.log(token);
-    if (!token) return;
-
-		try {
-			const res = await fetch(`http://localhost:8080/api/v1/messages/send/2`, {
-				method: "POST",
-
-				headers: {
-					"Authorization": token,
-          "Content-Type": "application/json"
-				},
-
-				body: JSON.stringify({message: "hi"}),
-			});
-
-      console.log(res);
-
-		} catch (error) {
-      console.error(error);
-    }
-  }
+  console.log(data)
 
   return (
     <div>
-      <button type='submit' onClick={handler}>클릭</button>
+      <button type='submit' onClick={mutate}>클릭</button>
       {/* <h1>채팅 목록 페이지입니다</h1> */}
       <Conversation/>
       {/* <Link to="/">홈으로</Link> */}
