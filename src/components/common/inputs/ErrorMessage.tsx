2a4967a938b3203f59d0bc23975d8ae428e6f3f7
parkhyeonwoo <pq120wal@naver.com>
2024-06-01 13:36:41 +0900

    design [박현우] : 에러메세지 디자인 변경

--- src/components/common/inputs/ErrorMessage.tsx
+++ src/components/common/inputs/ErrorMessage.tsx
@@ -7,5 +7,11 @@ export default function ErrorMessage({ children }: { children: React.ReactNode }
 }
 //TODO : 스타일링 추가 및 변경
 const Styled = {
-  MessageBox: styled.p``,
+  MessageBox: styled.p`
+    font-size: 12px;
+    color: red;
+    min-height: 12px;
+    min-width: 1px;
+    margin-top: 8px;
+  `,
 };
