d67eb134c2bfa2e9219316cc75e91d194d919818
sihyonn <sihyeon982@naver.com>
2024-06-01 20:08:47 +0900

    fix: id 타입 number -> string으로 변경

--- src/constants/API.ts
+++ src/constants/API.ts
@@ -8,30 +8,30 @@ export const API = Object.freeze({
 export const API_AUTH = Object.freeze({
   LOGIN: `${API.AUTH}/login`,
   JOIN: `${API.AUTH}/join`,
-  BY_ID: (userId: number) => `${API.AUTH}/${userId}`,
+  BY_ID: (userId: string) => `${API.AUTH}/${userId}`,
 });
 
 export const API_PICKLE = Object.freeze({
   CREATE: `${API.PICKLE}/create`,
-  BY_ID: (pickleId: number) => `${API.PICKLE}/${pickleId}`,
-  LIKE: (pickleId: number) => `${API.PICKLE}/${pickleId}/like`,
-  UNLIKE: (pickleId: number) => `${API.PICKLE}/${pickleId}/unlike`,
+  BY_ID: (pickleId: string) => `${API.PICKLE}/${pickleId}`,
+  LIKE: (pickleId: string) => `${API.PICKLE}/${pickleId}/like`,
+  UNLIKE: (pickleId: string) => `${API.PICKLE}/${pickleId}/unlike`,
   SEARCH: (pickleType: string) => `${API.PICKLE}/${pickleType}`, // 피클검색
   LOCATION_SEARCH: `${API.PICKLE}/search`, // 지역검색
   IN_LOCATION: `${API.PICKLE}/location`, // 지도 내 피클 조회
-  REVIEW: (pickleId: number) => `${API.PICKLE}/${pickleId}/review`,
-  ATTENDANCE: (pickleId: number) => `${API.PICKLE}/${pickleId}/public`,
-  RE_REQUEST: (pickleId: number) => `${API.PICKLE}/${pickleId}/reRequest`,
-  RE_REVIEW: (pickleId: number) => `${API.PICKLE}/${pickleId}/reReview`,
-  REGENERATE: (pickleId: number) => `${API.PICKLE}/${pickleId}/reReCruiting`, // 피클 재생성
+  REVIEW: (pickleId: string) => `${API.PICKLE}/${pickleId}/review`,
+  ATTENDANCE: (pickleId: string) => `${API.PICKLE}/${pickleId}/public`,
+  RE_REQUEST: (pickleId: string) => `${API.PICKLE}/${pickleId}/reRequest`,
+  RE_REVIEW: (pickleId: string) => `${API.PICKLE}/${pickleId}/reReview`,
+  REGENERATE: (pickleId: string) => `${API.PICKLE}/${pickleId}/reReCruiting`, // 피클 재생성
 
   // 내피클
-  MY_PICKEL: (userId: number) => `${API.PICKLE}/${userId}`,
-  MY_PICKEL_STATUS: (userId: number, status: string) => `${API.PICKLE}/${userId}/${status}`,
+  MY_PICKEL: (userId: string) => `${API.PICKLE}/${userId}`,
+  MY_PICKEL_STATUS: (userId: string, status: string) => `${API.PICKLE}/${userId}/${status}`,
 });
 
 export const API_CHAT = Object.freeze({
-  INQUIRY: (receiverId: number) => `${API.MESSAGES}/send/${receiverId}`,
-  LIST: (senderId: number) => `${API.MESSAGES}/${senderId}`,
-  GROUP: (pickleId: number, senderId: number) => `${API.CONVERSATIONS}/${pickleId}/${senderId}`,
+  INQUIRY: (receiverId: string) => `${API.MESSAGES}/send/${receiverId}`,
+  LIST: (senderId: string) => `${API.MESSAGES}/${senderId}`,
+  GROUP: (pickleId: string, senderId: string) => `${API.CONVERSATIONS}/${pickleId}/${senderId}`,
 });
