c9d2de6331ccb40746223ede00538360634a86ec
sihyonn <sihyeon982@naver.com>
2024-06-03 14:10:04 +0900

    feat: 찜 기능 핸들러 작성

--- src/mocks/handler/pickleHandlers.ts
+++ src/mocks/handler/pickleHandlers.ts
@@ -1,3 +1,19 @@
 import { http, HttpResponse } from 'msw';
 
-export const pickleHandlers = [];
+export const pickleHandlers = [
+  // 피클 찜하기
+  http.put('/pickle/:pickleId/scrap', ({ params }) => {
+    const { pickleId } = params;
+
+    if (!pickleId) return HttpResponse.json({ message: '존재하지 않는 피클입니다.' }, { status: 400 });
+    return HttpResponse.json({ message: '피클을 찜 했어요!' }, { status: 200 });
+  }),
+
+  // 피클 찜 취소
+  http.delete('/pickle/:pickleId/scrap', ({ params }) => {
+    const { pickleId } = params;
+
+    if (!pickleId) return HttpResponse.json({ message: '존재하지 않는 피클입니다.' }, { status: 400 });
+    return HttpResponse.json({ message: '찜이 취소되었습니다' }, { status: 200 });
+  }),
+];
