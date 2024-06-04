9be7418e7868cbd5b0d893d578ab94f77e747856
MinJoon_Choi <minjoon1998@gmail.com>
2024-06-04 20:40:08 +0900

    feat: CreatePickleData 타입 지정

--- src/apis/types/pickles.type.ts
+++ src/apis/types/pickles.type.ts
@@ -2,3 +2,17 @@ export interface Coordinates {
   latitude: number;
   longitude: number;
 }
+
+export type CreatePickleData = {
+  latitude: number;
+  longitude: number;
+  pickleType: string;
+  title: string;
+  startDate: string;
+  endDate: string;
+  price: number;
+  auth: number;
+  category: string;
+  capacity: number;
+  content: string;
+};
