51194fac85aa2afa20a474b3555bc4f37209b319
parkhyeonwoo <pq120wal@naver.com>
2024-06-05 15:09:50 +0900

    chore [박현우] : 피클 카드 컴포넌트 필요 이미지 및 아이콘 추가

---
+++ src/assets/icons/SpecialPickleCardArrowIcon.tsx
@@ -0,0 +1,10 @@
+export default function SpecialPickleCardArrowIcon() {
+  return (
+    <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M4.9668 8.32715L7.81055 5.4834H0.485352V4.48047H7.81055L4.9668 1.63672L5.67773 0.938477L9.71484 4.97559L5.67773 9.0127L4.9668 8.32715Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
