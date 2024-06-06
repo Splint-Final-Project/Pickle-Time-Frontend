d6bf70de679105b1fb42503d78cc121808e9a38a
sihyonn <sihyeon982@naver.com>
2024-06-06 22:26:10 +0900

    feat: useIntersectionObserver 훅 추가 및 무한스크롤 적용, 임시로더생성

--- src/components/picklecard/InfinitePickleCardList.tsx
+++ src/components/picklecard/InfinitePickleCardList.tsx
@@ -1,16 +1,36 @@
+import { useRef } from 'react';
+
 import WholePickleCard from '@/components/picklecard/WholePickleCard';
+import InfinitePickleCardLoader from '@/components/picklecard/InfinitePickleCardLoader';
+import useIntersectionObserver from '@/hooks/useIntersectionObserver';
 import { useGetInfinitePickles } from '@/hooks/query/pickles';
 import { WholePickle } from '@/apis/types/pickles.type';
 import { GridTemplate } from '@/styles/commonStyles';
 
 export default function InfinitePickleCardList() {
-  const { data: infiniteWholePickleData, fetchNextPage } = useGetInfinitePickles();
+  const loaderRef = useRef<HTMLElement>(null);
+  const { data: infiniteWholePickleData, fetchNextPage, hasNextPage } = useGetInfinitePickles();
+  const isLastPage = infiniteWholePickleData?.pages === infiniteWholePickleData?.pages;
+
+  useIntersectionObserver(async () => {
+    await fetchNextPage();
+  }, loaderRef);
 
   return (
-    <GridTemplate>
-      {infiniteWholePickleData?.pages.map(page =>
-        page?.data.map((pickle: WholePickle) => <WholePickleCard key={pickle.id} {...pickle} />),
-      )}
-    </GridTemplate>
+    <>
+      <GridTemplate>
+        {infiniteWholePickleData?.pages.map(page =>
+          page?.data.map((pickle: WholePickle) => <WholePickleCard key={pickle.id} {...pickle} />),
+        )}
+      </GridTemplate>
+      <InfinitePickleCardLoader
+        loaderRef={loaderRef}
+        style={
+          isLastPage && {
+            display: 'none',
+          }
+        }
+      />
+    </>
   );
 }
