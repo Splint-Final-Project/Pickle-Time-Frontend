c7ab32d31ce19b99c8927315221277e24c718dfd
sihyonn <sihyeon982@naver.com>
2024-06-07 20:28:07 +0900

    chore: 안 쓰이는 컴포넌트 제거

--- src/pages/profile/MyPage.tsx
+++ src/pages/profile/MyPage.tsx
@@ -1,7 +1,5 @@
 import SortButtons from '@/components/common/button/SortButtons';
 import InfinitePickleCardList from '@/components/picklecard/InfinitePickleCardList';
-import WholePickleCard from '@/components/picklecard/WholePickleCard';
-import { GridTemplate } from '@/styles/commonStyles';
 
 export default function MyPage() {
   return (
