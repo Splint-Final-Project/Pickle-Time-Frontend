29a4f80f880b78273d45c1d1af8f8afe4dd05fdc
sihyonn <sihyeon982@naver.com>
2024-06-08 22:46:54 +0900

    fix: 무한스크롤 버그 수정 및 임시 스켈레톤 적용

--- src/components/picklecard/InfinitePickleCardLoader.tsx
+++ src/components/picklecard/InfinitePickleCardLoader.tsx
@@ -1,3 +1,15 @@
-export default function InfinitePickleCardLoader({ loaderRef, ...props }) {
-  return <div>스켈렡톤 준비되는대로</div>;
+import WholePickleCardSkeleton from '@/components/picklecard/WholePickleCardSkeleton';
+
+interface InfinitePickleCardLoaderProps {
+  loaderRef: React.RefObject<HTMLDivElement>;
+  [key: string]: any;
+}
+export default function InfinitePickleCardLoader({ loaderRef, ...props }: InfinitePickleCardLoaderProps) {
+  return (
+    <div ref={loaderRef} {...props}>
+      {Array.from({ length: 5 }).map((_, index) => (
+        <WholePickleCardSkeleton key={index} />
+      ))}
+    </div>
+  );
 }
---
+++ src/components/picklecard/WholePickleCardSkeleton.tsx
@@ -0,0 +1,3 @@
+export default function WholePickleCardSkeleton() {
+  return <div>스케레레레레</div>;
+}
