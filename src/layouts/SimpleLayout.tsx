6d3e4fcf9915843623f8a379de6a8e7ba4912bf2
MinJoon_Choi <minjoon1998@gmail.com>
2024-06-08 22:57:07 +0900

    feat: bottomNav 필요 없는 화면을 위한 레이아웃 추가 구현

---
+++ src/layouts/SimpleLayout.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import { Outlet } from 'react-router-dom';
+
+const breakpoints = {
+  mobile: '768px',
+};
+
+const S = {
+  LayoutContainer: styled.div`
+    display: flex;
+    flex-direction: column;
+    min-height: 100dvh;
+    border-left: 0.1px solid #f0f0f0;
+    border-right: 0.1px solid #f0f0f0;
+    width: 100%;
+
+    @media (min-width: ${breakpoints.mobile}) {
+      max-width: 767px;
+      margin: 0 auto;
+    }
+  `,
+  Content: styled.main`
+    flex: 1;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  `,
+};
+
+interface SimpleLayoutProps {
+  children?: React.ReactNode;
+}
+
+export default function SimpleLayout({ children }: SimpleLayoutProps) {
+  return (
+    <S.LayoutContainer>
+      <S.Content>
+        <Outlet />
+        {children}
+      </S.Content>
+    </S.LayoutContainer>
+  );
+}
