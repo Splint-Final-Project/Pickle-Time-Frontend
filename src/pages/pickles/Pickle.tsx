85510744bd09fdd8f2ab67001ecb2beb9a9da038
MinJoon_Choi <minjoon1998@gmail.com>
2024-06-08 22:55:39 +0900

    chore: 지도 pickle 화면에 적용

--- src/pages/pickles/Pickle.tsx
+++ src/pages/pickles/Pickle.tsx
@@ -1,6 +1,12 @@
+import KaKaoMap from '@/components/map/KaKaoMap';
 import React from 'react';
 
 export default function Pickle() {
   // 파라미터로 받은 피클 id로 피클 정보를 가져와서 보여줌
-  return <div>파라미터로 받은 피클 id로 피클 정보를 가져와서 보여줌</div>;
+  return (
+    <div>
+      <p>파라미터로 받은 피클 id로 피클 정보를 가져와서 보여줌</p>
+      <KaKaoMap />
+    </div>
+  );
 }
