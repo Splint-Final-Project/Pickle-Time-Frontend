e4d5a3623f640e5b326ff48baac837ff9d7b1712
sihyonn <sihyeon982@naver.com>
2024-06-11 17:18:06 +0900

    refactor: 별점선택 개별 컴포넌트로 분리

---
+++ src/components/my-page/review/StarRating.tsx
@@ -0,0 +1,31 @@
+import StarIcon from '@/assets/icons/StarIcon';
+import styled from '@emotion/styled';
+
+export type Rating = number;
+interface RatingProps {
+  selectedRating: number;
+  onStarHover: (rating: Rating) => void;
+  onStarClick: (rating: Rating) => void;
+}
+
+export default function StarRating({ selectedRating, onStarHover, onStarClick }: RatingProps) {
+  return (
+    <S.Rating>
+      {[1, 2, 3, 4, 5].map(rating => (
+        <StarIcon
+          key={rating}
+          filled={rating <= selectedRating}
+          onMouseEnter={() => onStarHover(rating)}
+          onClick={() => onStarClick(rating)}
+        />
+      ))}
+    </S.Rating>
+  );
+}
+
+const S = {
+  Rating: styled.div`
+    display: flex;
+    gap: 0.7rem;
+  `,
+};
