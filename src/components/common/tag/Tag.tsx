b7083ce176e70b4006ee7da1c2037e36b46ca69d
whai2 <whai2125@gmail.com>
2024-06-13 17:17:43 +0900

    style: 핸들러 유무를 통해, 스타일 분기

--- src/components/common/tag/Tag.tsx
+++ src/components/common/tag/Tag.tsx
@@ -1,40 +1,37 @@
-import { ReactNode } from "react"
+import { ReactNode } from 'react';
 import TagDeleteIcon from '/public/icons/TagDelete.svg';
-import styled from "@emotion/styled"
+import styled from '@emotion/styled';
 
 interface TagInterface {
-  children: ReactNode,
-  handleRemove?: () => void,
-  hasHandler : boolean,
+  children: ReactNode;
+  handleRemove?: () => void;
+  hasHandler: boolean;
 }
 
 export default function Tag({ children, handleRemove, hasHandler }: TagInterface) {
   return (
-    <S.Container>
+    <S.Container hasHandler={hasHandler}>
       <S.Text>{children}</S.Text>
-      {hasHandler ? (
-        <S.IconImg src={TagDeleteIcon} onClick={handleRemove}/>
-      ): (
-        null
-      )}
+      {hasHandler ? <S.IconImg src={TagDeleteIcon} onClick={handleRemove} /> : null}
     </S.Container>
-  )
+  );
 }
 
 const S = {
-  Container: styled.div`
+  Container: styled.div<{ hasHandler: boolean }>`
     display: inline-flex;
     align-items: center;
     justify-content: space-between;
-    height: 3.0rem;
+    height: 3rem;
     flex-shrink: 0;
     border-radius: 0.4rem;
-    border: 0.1rem solid #5DC26D;
+    border: ${({ hasHandler }) => (hasHandler ? '0.1rem solid rgba(93, 194, 109, 1)' : '')};
     padding: 1rem;
+    background-color: ${({ hasHandler }) => (hasHandler ? 'rgba(255, 255, 255, 0)' : 'rgba(93, 194, 109, 0.15)')};
   `,
 
   Text: styled.span`
-    color: #5DC26D;
+    color: #5dc26d;
     font-family: Pretendard;
     font-size: 1.2rem;
     font-weight: 500;
@@ -48,5 +45,5 @@ const S = {
     flex-shrink: 0;
     margin-left: 0.5rem; /* 아이콘과 텍스트 사이의 간격 조정 */
     cursor: pointer; /* 클릭 가능한 아이콘을 위한 커서 변경 */
-  `
-}
+  `,
+};
