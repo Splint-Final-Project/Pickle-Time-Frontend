880b567c5919bf14ceab7537cc87059185089b04
sihyonn <sihyeon982@naver.com>
2024-06-13 20:30:35 +0900

    feat: 하트아이콘 + 좋아요 수를 관리하는 LikeCount 생성

---
+++ src/components/pickle-detail/LikeCount.tsx
@@ -0,0 +1,36 @@
+import styled from '@emotion/styled';
+import HeartButton from '@/components/common/button/HeartButton';
+import useHeartButtonClick from '@/hooks/useHeartButtonClick';
+
+interface LikeCountProps {
+  pickleId: string;
+  likeCount: number;
+}
+
+export default function LikeCount({ pickleId, likeCount = 0 }: LikeCountProps) {
+  const { isLiked, handleHeartClick } = useHeartButtonClick(pickleId);
+
+  const updateLikeCount = () => {
+    if (isLiked) {
+      return likeCount + 1;
+    }
+    return likeCount;
+  };
+
+  return (
+    <S.Container>
+      <HeartButton size={20} isActive={isLiked} onClick={handleHeartClick} />
+      <S.LikeCount>{updateLikeCount()}</S.LikeCount>
+    </S.Container>
+  );
+}
+
+const S = {
+  Container: styled.div`
+    display: flex;
+    align-items: center;
+  `,
+  LikeCount: styled.span`
+    ${({ theme }) => theme.typography.body1}
+  `,
+};
