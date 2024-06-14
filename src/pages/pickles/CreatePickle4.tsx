c3ef9f0dc465b97c90c0d500e32bd6220effeefe
parkhyeonwoo <pq120wal@naver.com>
2024-06-14 14:22:22 +0900

    feat [박현우] : 피클 생성-4페이지 1차 구현

--- src/pages/pickles/CreatePickle4.tsx
+++ src/pages/pickles/CreatePickle4.tsx
@@ -1,94 +1,70 @@
-import client from '@/apis/axios';
 import useAuth from '@/hooks/zustand/useAuth';
 import usePickleCreation from '@/hooks/zustand/usePickleCreation';
-import React, { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import {
-  Container,
-  InputComponent,
-  StepIndicator,
-  StepIndicatorContainer,
-  SubmitButton,
-  Title,
-  TitleContainer,
-} from './CreatePickleStyled';
+import { Container, StepIndicator, StepIndicatorContainer, Title, TitleContainer } from './CreatePickleStyled';
+import PaymentWindow from '@/components/picklePayment/PaymentComponent';
+import styled from '@emotion/styled';
+import { useDateSelect } from '@/hooks/zustand/useDateSelect';
+import paymentCall, { PaymentDataType } from '@/utils/paymentCall';
 
 export default function CreatePickle4() {
-  const { IMP } = window;
   const { getMe } = useAuth();
   const user = getMe();
   const navigate = useNavigate();
-  const [paymentMethod, setPaymentMethod] = useState<string>('kakaopay');
-  const { title, capacity, cost, deadLine, where, when, category, explanation, viewCount, latitude, longitude, clear } =
-    usePickleCreation();
-  if (
-    !title ||
-    !capacity ||
-    !cost ||
-    // !deadLine ||
-    // !where ||
-    // !when ||
-    !category ||
-    !explanation
-    // !viewCount ||
-    // !latitude ||
-    // !longitude
-  ) {
-    return <div>피클 정보가 부족합니다.</div>;
-  }
-  function onClickPayment() {
-    const data = {
+  const [paymentMethod, setPaymentMethod] = useState<string>('');
+  const [usePointValue, setUsePointValue] = useState(0);
+  const [isAgree, setIsAgree] = useState(false);
+  const { title, capacity, cost, category, explanation, clear, imgUrl } = usePickleCreation();
+  const { startDate, finishDate, weekend, startTime, finishTime } = useDateSelect();
+  const paymentData: PaymentDataType = useMemo(
+    () => ({
       pg: `${paymentMethod === 'kakaopay' ? 'kakaopay.TC0ONETIME' : 'tosspay.tosstest'}`,
       pay_method: 'card',
-      merchant_uid: `mid_${new Date().getTime()}`, // 해당 피클의 아이디?
-      amount: cost,
+      merchant_uid: `mid_${new Date().getTime()}`,
+      amount: 500,
       name: `${title} 생성하기`,
-      buyer_name: user.name,
+      buyer_name: user.nickname,
       custom_data: {
         title,
         capacity,
-        cost,
-        deadLine,
-        where,
-        when,
         category,
         explanation,
-        viewCount,
-        latitude,
-        longitude,
       },
       m_redirect_url: `${window.location.origin.toString()}/create-redirect`,
-    };
+    }),
 
-    IMP.init('imp88171622');
+    [paymentMethod, usePointValue],
+  );
 
-    IMP.request_pay(data, async (response: any) => {
-      if (!response.success) {
-        alert(`결제에 실패했습니다: ${response.error_msg}`);
-        navigate(`/pickle-create`, { replace: true });
-      }
+  const pickleData = useMemo(
+    () => ({
+      category,
+      imgUrl,
+      title,
+      price: cost,
+      day: weekend.filter(day => day.isClicked).map(day => day.day),
+      capacity,
+      duration: `${startDate.month}/${startDate.day} - ${finishDate.month}/${finishDate.day}`,
+      time: `${startTime.hour} : ${startTime.minute}${startTime.dayTime === 'AM' ? 'am' : 'pm'} ~ ${finishTime.hour} : ${finishTime.minute}${finishTime.dayTime === 'AM' ? 'am' : 'pm'} `,
+    }),
+    [],
+  );
 
-      const notified = await client.post('/pickle/create', {
-        imp_uid: response.imp_uid,
-      });
+  const successPayment = () => {
+    alert('성공');
+    clear();
+  };
 
-      // 결제 성공(피클도 생성됨) 결과에 따라 분기
-      console.log(notified);
-      if (notified.status === 201) {
-        alert('결제 및 피클 생성이 완료되었습니다.');
-        clear();
-        navigate(`/pickle/${notified.data.pickle._id}`, { replace: true });
-      } else {
-        alert('피클 생성이 실패하여 결제 금액은 환불되었습니다.' + notified.data.message);
-        navigate(`/pickle-create`, { replace: true });
-      }
-    });
-  }
+  const errorPayment = (message: string) => {
+    alert(message);
+  };
+
   return (
     <Container>
       <TitleContainer>
         <Title>
-          <img src="icons/back.svg" alt="back" onClick={() => navigate('/pickle-create-1')} />
+          <img src="icons/back.svg" alt="back" onClick={() => navigate('/pickle-create-3')} />
           <div>피클 생성</div>
         </Title>
         <StepIndicatorContainer>
@@ -98,34 +74,63 @@ export default function CreatePickle4() {
           <StepIndicator $selected={true}>4</StepIndicator>
         </StepIndicatorContainer>
       </TitleContainer>
-
-      <InputComponent>
-        {' '}
-        <span>
-          <input
-            type="radio"
-            id="kakao"
-            name="paymentType"
-            value="kakaopay"
-            onChange={e => setPaymentMethod(e.target.value)}
-          />
-          <label htmlFor="kakao">카카오페이</label>
-        </span>
-        <span>
-          <input
-            type="radio"
-            id="toss"
-            name="paymentType"
-            value="tosspay"
-            onChange={e => setPaymentMethod(e.target.value)}
-          />
-          <label htmlFor="toss">토스페이</label>
-        </span>{' '}
-      </InputComponent>
-      <InputComponent></InputComponent>
-      <InputComponent></InputComponent>
-      <InputComponent></InputComponent>
-      <SubmitButton onClick={onClickPayment}>{cost}원 결제하기</SubmitButton>
+      <PaymentWindow.Section>
+        <PaymentWindow.PreviewPickle data={pickleData} type="create" />
+      </PaymentWindow.Section>
+      <PaymentWindow.Section>
+        {/* TODO :포인트 api연결하기 */}
+        <PaymentWindow.Point totalPoint={1500} setUsePoint={setUsePointValue} />
+      </PaymentWindow.Section>
+      <PaymentWindow.Section>
+        <PaymentWindow.FinalAmount total={cost} usePoint={usePointValue} />
+      </PaymentWindow.Section>
+      <PaymentWindow.Section>
+        <PaymentWindow.Methods setState={setPaymentMethod} />
+      </PaymentWindow.Section>
+      <PaymentWindow.Section>
+        <PaymentWindow.PaymentEvent />
+      </PaymentWindow.Section>
+      <PaymentWindow.Section>
+        <PaymentWindow.PaymentTerms setState={setIsAgree} />
+      </PaymentWindow.Section>
+      <S.Wrap>
+        <S.Notice>* 2주 이내 모집이 완료되지 않으면 피클은 사라집니다.</S.Notice>
+        <S.Notice>* 사라진 피클은 입금 계좌로 영업일 2~3일 이내 환불됩니다.</S.Notice>
+      </S.Wrap>
+      <S.PaymentButton
+        onClick={() =>
+          paymentCall({ paymentData: paymentData, successCallback: successPayment, errorCallback: errorPayment })
+        }
+        disabled={!paymentMethod || !isAgree}
+      >
+        {cost - usePointValue}원 결제하기
+      </S.PaymentButton>
     </Container>
   );
 }
+
+const S = {
+  Wrap: styled.div`
+    padding: 1.6rem 2rem;
+  `,
+  Notice: styled.p`
+    color: #8b8d94;
+    font-size: 1.2rem;
+    &:nth-of-type(1) {
+      margin-bottom: 0.8rem;
+    }
+  `,
+  PaymentButton: styled.button`
+    margin: 0 20px;
+    height: 42px;
+    border-radius: 4px;
+    background: var(--Main-Color, #5dc26d);
+    color: white;
+    font-size: 1.4rem;
+
+    &:disabled {
+      background: #d0d0d0;
+      cursor: auto;
+    }
+  `,
+};
