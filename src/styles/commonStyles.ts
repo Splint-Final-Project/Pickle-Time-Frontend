9caa521e411cf70b5d321fb1df8164336d20cb32
MinJoon_Choi <minjoon1998@gmail.com>
2024-06-15 18:04:18 +0900

    feat: two columns grid 추가

--- src/styles/commonStyles.ts
+++ src/styles/commonStyles.ts
@@ -10,3 +10,20 @@ export const GridTemplate = styled.div`
   grid-template-columns: repeat(auto-fill, minmax(15rem, 1fr));
   grid-gap: 2rem;
 `;
+
+export const TwoColumnGridTemplate = styled.div`
+  display: grid;
+  justify-content: center;
+  align-items: flex-start;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 2rem;
+  margin-top: 2rem;
+  margin-bottom: 2rem;
+  grid-auto-rows: minmax(15rem, 1fr);
+
+  @media (max-width: 500px) {
+    grid-gap: 1.5rem;
+    margin-top: 1.5rem;
+    margin-bottom: 1.5rem;
+  }
+`;
