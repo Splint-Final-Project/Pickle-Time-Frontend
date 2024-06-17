1bbf86dc0024b3cc8495d50078e6b039254459d0
parkhyeonwoo <pq120wal@naver.com>
2024-06-17 15:43:14 +0900

    feat [박현우] : 내피클 페이지 레이아웃 구성 및 오늘의 피클 섹션 추가

---
+++ src/pages/pickles/MyPickles.tsx
@@ -0,0 +1,37 @@
+import TodayPickleListContainer from '@/components/myPicklePage/TodayPickleListContainer';
+import styled from '@emotion/styled';
+
+export default function MyPickles() {
+  return (
+    <S.Container>
+      <S.Title>내 피클</S.Title>
+      <S.TodayPickleSection>
+        <TodayPickleListContainer />
+      </S.TodayPickleSection>
+      <S.Line />
+      <S.MyPickleListSection></S.MyPickleListSection>
+    </S.Container>
+  );
+}
+
+const S = {
+  Container: styled.div`
+    padding: 8rem 0 9.6rem;
+  `,
+  Title: styled.h1`
+    font-size: 2.4rem;
+    font-weight: 600;
+    margin-bottom: 1rem;
+    padding: 0 1.6rem;
+  `,
+  TodayPickleSection: styled.section`
+    padding: 0 1.6rem 3.2rem;
+  `,
+  Line: styled.div`
+    height: 1.2rem;
+    background: #f6f6f6;
+  `,
+  MyPickleListSection: styled.section`
+    padding: 2.8rem 1.6rem 0;
+  `,
+};
