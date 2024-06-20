d9803f786acdefb790cf71ce17366c7620e506b4
sihyonn <sihyeon982@naver.com>
2024-06-21 04:22:00 +0900

    refactor: 프로필 수정페이지 활동지역 설정 AreaSetting 컴포넌트로 분리

---
+++ src/components/my-page/edit/AreaSetting.tsx
@@ -0,0 +1,45 @@
+import styled from '@emotion/styled';
+import AreaSelectModal from '@/components/common/modal/AreaSelectModal';
+import ActivityArea from '@/components/my-page/edit/ActivityArea';
+import useBottomSheetModal from '@/hooks/zustand/useBottomSheetModal';
+
+export default function AreaSetting({ profileState, setProfileState }) {
+  const { handleOpen } = useBottomSheetModal(state => state);
+  const handleAreaSelectComplete = (newAreaCodes: number[]) => {
+    setProfileState({ ...profileState, areaCodes: newAreaCodes });
+  };
+
+  return (
+    <>
+      <S.AreaSettingContainer>
+        활동 범위 설정하기
+        <button
+          onClick={() =>
+            handleOpen({
+              renderComponent: AreaSelectModal,
+              areaCodes: profileState.areaCodes,
+              onComplete: handleAreaSelectComplete,
+            })
+          }
+        >
+          <img src="/icons/rightArrowIcon.svg" />
+        </button>
+      </S.AreaSettingContainer>
+      <ActivityArea areaCodes={profileState.areaCodes} />
+    </>
+  );
+}
+
+const S = {
+  AreaSettingContainer: styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    padding: 1.4rem;
+    margin-bottom: 2.1rem;
+    border-radius: 0.8rem;
+    background-color: ${({ theme }) => theme.color.secondary2};
+    ${({ theme }) => theme.typography.body3};
+  `,
+};
