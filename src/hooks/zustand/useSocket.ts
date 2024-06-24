bd1418ff7a48a999aa472bfc5e611145a89e39e2
whai2 <whai2125@gmail.com>
2024-06-25 02:09:35 +0900

    feat: credentials  true

--- src/hooks/zustand/useSocket.ts
+++ src/hooks/zustand/useSocket.ts
@@ -18,6 +18,7 @@ const useSocket = create<SocketType>((set, get) => ({
         timeout: 5000, // 타임아웃 설정 (예: 5초)
         reconnectionAttempts: 5, // 재연결 시도 횟수 제한
         transports: ['websocket'], // 사용할 전송 프로토콜 지정
+        withCredentials: true
       });
 
       // socket.on("getOnlineUsers", (users) => {
