f953fddf92fc5270b4f088b251b41c61ec994fc5
whai2 <whai2125@gmail.com>
2024-06-24 22:41:24 +0900

    fix

--- src/hooks/zustand/useSocket.ts
+++ src/hooks/zustand/useSocket.ts
@@ -11,7 +11,7 @@ const useSocket = create<SocketType>((set, get) => ({
   socket: null,
   initializeSocket: (authUserId: string) => {
     if (authUserId) {
-      const socket = io("wss://pickle-time.net/socket.io/", {
+      const socket = io(`${import.meta.env.VITE_BACKEND_URL}`, {
         query: {
           userId: authUserId,
         },
